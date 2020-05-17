5effe04a0c653ecba030f027fb24646fcfa6c786
Volodymyr <lesiyk47@gmail.com>
2020-05-17 20:31:16 +0300

    Presentational and Container Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, CardImgOverlay } from 'reactstrap';
 
 class DishDetail extends React.Component {
   
@@ -8,25 +8,30 @@ class DishDetail extends React.Component {
       let dishComments = this.props.dish.comments.map( ( comment ) => {
         return (
           <div key={comment.id}>
-            {comment.comment}<br /> <div class="mb-3">--{comment.author}, {comment.date.substring(0,10)}</div>
+            {comment.comment}<br /> <div class="mb-3">--{comment.author}, 
+            {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</div>
           </div>
         );
       });  
         return (
-          <div className="row" >
-            <div className="col-12 col-md-5 m-1" >
-              <Card>
-                <CardImg top width="100%" src={this.props.dish.image} alt={this.props.dish.name} />
-                <CardBody>
-                  <CardTitle>{this.props.dish.name}</CardTitle>
-                  <CardText>{this.props.dish.description}</CardText>
-                </CardBody>
-              </Card>
-            </div>
-            <div className="col-12 col-md-5 m-1">
-              <h4>Comments</h4>
-              <div>
-                {dishComments}
+          <div className="container" >
+            <div className="row" >
+              <div className="col-12 col-md-5 m-1" >
+                <Card>
+                  <CardImg top width="100%" src={this.props.dish.image} alt={this.props.dish.name} />
+                  <CardBody>
+                    <CardImgOverlay>
+                      <CardTitle>{this.props.dish.name}</CardTitle>
+                    </CardImgOverlay>
+                    <CardText>{this.props.dish.description}</CardText>
+                  </CardBody>
+                </Card>
+              </div>
+              <div className="col-12 col-md-5 m-1">
+                <h4>Comments</h4>
+                <div>
+                  {dishComments}
+                </div>
               </div>
             </div>
           </div>
