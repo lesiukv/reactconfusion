6fd4bcffc1ac9244babf39302606ef9aa71bd01f
Volodymyr <lesiyk47@gmail.com>
2021-06-22 18:05:20 +0300

    errors and stuff OMEGALUL

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -45,11 +45,14 @@ class Main extends Component {
     const HomePage = () => {
       return(
         <Home 
-          dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
           dishesLoading={this.props.dishes.isLoading}
+
           dishErrMess={this.props.dishes.errMess}
+
           promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
+
           promoLoading={this.props.promotions.isLoading}
+
           promoErrMess={this.props.promotions.errMess}
           leader={this.props.leaders.filter((leader) => leader.featured)[0]}
         />
