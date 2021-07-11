5348fa1ec71b6247ad713133fafdd86f35afcda4
Volodymyr <lesiyk47@gmail.com>
2021-07-11 18:23:48 +0300

    Transition and Post Comments

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -8,42 +8,39 @@ export const addComment = (comment) => ({
 });
 
 export const postComment = (dishId, rating, author, comment) => (dispatch) => {
+
     const newComment = {
         dishId: dishId,
         rating: rating,
         author: author,
         comment: comment
-    }
-    newComment.date = new Date().toISOSstring();
-
+    };
+    newComment.date = new Date().toISOString();
+    
     return fetch(baseUrl + 'comments', {
-        method: 'POST',
+        method: "POST",
         body: JSON.stringify(newComment),
         headers: {
-            'Conetent-Type': 'application/json'
+          "Content-Type": "application/json"
         },
-        credentials: 'same-origin'
+        credentials: "same-origin"
     })
-        .then(response => {
-            if (response.ok) {
-                return response;
-            } 
-            else { 
-                var error = new Error('Error ' + response.status + '; ' + response.statusText);
-                error.response = response; 
-                throw error;
-            }
-        },
-        error => {
-            var errmess = new Error(error.message);
-            throw errmess;
-        })
-        .then(response => response.json())
-        .then(response => dispatch(addComment(response)))
-        .catch(error => { console.log('Post comments', error.message,
-            alert('Your comment could not be posted\nError'+ error.message))})
-
-}
+    .then(response => {
+        if (response.ok) {
+          return response;
+        } else {
+          var error = new Error('Error ' + response.status + ': ' + response.statusText);
+          error.response = response;
+          throw error;
+        }
+      },
+      error => {
+            throw error;
+      })
+    .then(response => response.json())
+    .then(response => dispatch(addComment(response)))
+    .catch(error =>  { console.log('post comments', error.message); alert('Your comment could not be posted\nError: '+error.message); });
+};
 
 
 export const fetchDishes = () => (dispatch) => {
